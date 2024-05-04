7c043acb2cb05ae885b53d32e31e7b1bceed7c0c
Dmitry <tina8069@yandex.ru>
2024-05-04 15:53:23 +0300

    now validateEnvVariables sets default schema values on validate

--- src/utils/validateEnvVariables.ts
+++ src/utils/validateEnvVariables.ts
@@ -1,9 +1,18 @@
 import { envSchema } from '../schemas/env.schema';
 
+/**
+ * Validates the environment variables against the provided schema.
+ *
+ * @return {Number} - Throws an error if the environment variables are invalid.
+ */
 export function validateEnvVariables() {
-  const { error } = envSchema.validate(process.env);
+  const { error, value } = envSchema.validate(process.env);
 
   if (error) {
     throw new Error(`Environment validation error: ${error.message}`);
   }
+
+  Object.keys(envSchema.describe().keys).forEach((key) => {
+    process.env[key] = value[key];
+  });
 }
