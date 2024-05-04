f81b4c20fe3006574b686bc871f5dc241f3e9e92
Dmitry <tina8069@yandex.ru>
2024-05-04 13:24:14 +0300

    added routes validators

---
+++ src/middleware/validateBody.middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express'
+import Joi from 'joi'
+
+export const validateBody =
+  (schema: Joi.ObjectSchema) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body)
+
+    if (!error) {
+      next()
+    } else {
+      const { details } = error
+      const message = details.map((i) => i.message).join(', ')
+
+      console.log('error', message)
+
+      res.status(400).json({ message })
+    }
+  }
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express'
 import { UserController } from '../controllers/user.controller'
+import { validateBody } from '../middleware/validateBody.middleware'
+import { loginSchema, registerSchema } from '../schemas/auth.schema'
 
 const router = Router()
 
-router.post('/login', UserController.login)
+router.post('/login', validateBody(loginSchema), UserController.login)
 
 router.get('/logout', UserController.logout)
 
-router.post('/register', UserController.register)
+router.post('/register', validateBody(registerSchema), UserController.register)
 
 router.delete('/remove', UserController.remove)
 
